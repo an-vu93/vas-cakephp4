5292f1f374bde90862a955629717db4ef62c9638
an-vu@ds-brand.jp <an-vu>
2024-10-03 10:42:23 +0900

    Base cakephp4 project with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.php",
+    "./src/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
