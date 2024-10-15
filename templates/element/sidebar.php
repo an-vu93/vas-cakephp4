4b379ac530bd12712092d0ee5754c6294783b85f
an-vu@ds-brand.jp <an-vu>
2024-10-15 14:34:33 +0900

    Update sidebar link

--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -2,7 +2,10 @@
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
         <ul class="space-y-2 font-medium">
             <?= $this->element('sidebar/list_item', [
-                'link' => '#',
+                'link' => $this->Url->build([
+                    'controller' => 'Search',
+                    'action' => 'index',
+                ]),
                 'icon' => '<path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>',
                 'text' => 'Home',
             ]); ?>
