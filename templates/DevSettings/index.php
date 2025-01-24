c3f9f553b42346c762acff9cc121cee464186734
an-vu@ds-brand.jp <an-vu>
2025-01-24 16:04:37 +0900

    Add link to CMS flag settings in DevSettings template

--- templates/DevSettings/index.php
+++ templates/DevSettings/index.php
@@ -36,6 +36,21 @@ $this->assign('title', '開発者メニュー');
                 </div>
             </a>
         </div>
+
+        <div class="w-96">
+            <a href="<?= $this->Url->build([
+                    'controller' => 'OricohSeries',
+                    'action' => 'index',
+                ]) ?>" 
+                class="block p-5 text-base font-medium text-gray-500 rounded-lg bg-primary-100 hover:text-gray-900 hover:bg-primary-200 transition-colors">
+                <div class="flex items-center justify-between">
+                    <span>CMSフラグ設定</span>
+                    <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
